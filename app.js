74d9f88456042e27808dfcdf6a6624c7d3b26c9e
kridi32 <kridi32@outlook.com>
2022-02-09 14:05:45 +0600

    click function added

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const tr = document.getElementById("tr");
 const td = document.getElementById("data");
+// lets get the body
+const tableBody = document.getElementById("tableBody");
+function taskMaker(rows){
+    rows.appendChild(sl());
+    rows.appendChild(taskName());
+    rows.appendChild(taskStatus());
+    return rows;
+}
+function sl(){
+    let serial = document.createElement("td");
+    serial.innerText = "1";
+    serial.classList = "px-12 py-2 bg-purple-200  text-2xl font-semibold";
+    return serial;
+}
+function taskName(){
+    let task = document.createElement("td");
+    task.innerText =  "do something new";
+    task.classList = "px-auto py-auto bg-purple-200  text-lg font-regular";
+    return task;
+}
+function taskStatus(){
+    let task = document.createElement("td");
+    task.innerText =  "done";
+    task.classList = "px-auto py-auto bg-purple-200  text-xl font-bold";
+    return task;
+}
 td.addEventListener("click", function(){
-    let ourTd = document.createElement("td");
-    ourTd.classList = "px-12 bg-red-200 text-2xl font-semibold border-b-2 border-red-600";
-    ourTd.style.margin = "20px 30px";
-    ourTd.innerText = "other";
-    tr.appendChild(ourTd);
+    
+let singleTask = document.createElement("tr");
+   tableBody.appendChild(taskMaker(singleTask));
 });
